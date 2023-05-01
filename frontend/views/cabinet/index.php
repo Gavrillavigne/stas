79b8c64f6115f016fe4013ab8e9848361c8d1b92
Stanislav Gavrilchik <dev_gs@bankiros.ru>
2023-05-01 15:06:10 +0300

    feat(common): add share file option PHP-19 - add share file option

--- frontend/views/cabinet/index.php
+++ frontend/views/cabinet/index.php
@@ -7,6 +7,8 @@
 <html xmlns="http://www.w3.org/1999/html">
 <head>
     <title>Personal Cabinet</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="../../frontend/web/js/share-file.js"></script>
     <meta charset="utf-8"/>
 </head>
 <body>
@@ -14,16 +16,36 @@
 
 <div>
     <p>Список загруженных файлов:</p>
+    <hr>
     <?php foreach ($files as $id => $file) {
+        $url = !empty($file['filePath']) ? getenv('DOMAIN') . '/' . $file['filePath'] : '';
+        $title = $file['fileName'] ?? '';
+        $path = !empty($file['filePath']) ? getenv('DOMAIN') . '/' . $file['filePath'] : '';
+        $desc = $file['fileName'] ?? '';
+
         echo $file['fileName'] ?>
         <form id="delete-<?php echo $id; ?>" method="post">
             <input type="hidden" name="delete" value="<?php echo $id; ?>">
             <input type="submit" value="Удалить"/>
             <a href="<?php echo $file['filePath']; ?>" download="<?php echo $file['fileName']; ?>">Скачать</a>
-<!--            <a href="--><?php //echo $file['filePath']; ?><!--" target="_blank">Открыть</a>-->
+            <!--            <a href="--><?php //echo $file['filePath']; ?><!--" target="_blank">Открыть</a>-->
             <a href="<?php echo 'cabinet/open-file/' . $file['filePath']; ?>" target="_blank">Открыть</a>
         </form>
-        <br/>
+        <br>
+        <div>
+            <span>Поделиться файлом:</span><br>
+            <a onclick='<?php echo 'Share.vkontakte("' . $url . '","' . $title . '","' . $path . '","' . $desc . '")'; ?>'>
+                <i class="fa fa-vk"></i></a>
+            <a onclick='<?php echo 'Share.facebook("' . $url . '","' . $title . '","' . $path . '","' . $desc . '")'; ?>'>
+                <i class="fa fa-facebook"></i></a>
+            <a onclick='<?php echo 'Share.mailru("' . $url . '","' . $title . '","' . $path . '","' . $desc . '")'; ?>'>
+                {MailRu}</a>
+            <a onclick='<?php echo 'Share.odnoklassniki("' . $url . '","' . $title . '","' . $path . '","' . $desc . '")'; ?>'>
+                <i class="fa fa-odnoklassniki"></i></a>
+            <a onclick='<?php echo 'Share.twitter("' . $url . '","' . $title . '","' . $path . '","' . $desc . '")'; ?>'>
+                <i class="fa fa-twitter"></i></a>
+        </div>
+        <hr>
     <?php } ?>
 </div>
 <hr>
